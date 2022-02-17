1ca036b3766e674c778fb0c00006a4f0395c38d2
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-02-17 16:37:46 -0700

    started working on wheel

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import * as actionCreators from '../state/action-creators'
 
-export default function Wheel(props) {
+export function Wheel(props) {
+  const { wheel_id } = props
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+       {wheel_id===0?<div className="cog active" style={{ "--i": 0 }}>B</div>:<div className="cog"></div>}
+       {wheel_id===1?<div className="cog active" style={{ "--i": 1 }}>B</div>:<div className="cog"></div>}
+       {wheel_id===2?<div className="cog active" style={{ "--i": 2 }}>B</div>:<div className="cog"></div>}
+       {wheel_id===3?<div className="cog active" style={{ "--i": 3 }}>B</div>:<div className="cog"></div>}
+       {wheel_id===4?<div className="cog active" style={{ "--i": 4 }}>B</div>:<div className="cog"></div>}
+       {wheel_id===5?<div className="cog active" style={{ "--i": 5 }}>B</div>:<div className="cog"></div>}
+        {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
         <button id="counterClockwiseBtn" >Counter clockwise</button>
@@ -18,3 +22,5 @@ export default function Wheel(props) {
     </div>
   )
 }
+
+export default connect(state => state, actionCreators)(Wheel)

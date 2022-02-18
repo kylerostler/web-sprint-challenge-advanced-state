3c37c14c1c7745376b71a91c60a9aad407dd0067
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-02-18 10:08:09 -0700

    worked on quiz

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -24,23 +24,35 @@ export function Quiz(props) {
         // quiz already in state? Let's use that, otherwise render "Loading next quiz..."
         quiz ? (
           <>
-            <h2>What is a closure?</h2>
+            <h2>{quiz.question}</h2>
 
-            <div id="quizAnswers">
-              <div className="answer selected">
-                A function
+              {/* {selected?<div className="answer selected">
+                {quiz.answers[0].text}
                 <button>
                   SELECTED
                 </button>
               </div>
-
+              :
               <div className="answer">
-                An elephant
+                {quiz.answers[0].text}
                 <button>
                   Select
                 </button>
+              </div>}
+
+              {selected?<div className="answer selected">
+                {quiz.answers[1].text}
+                <button>
+                  SELECTED
+                </button>
               </div>
-            </div>
+              :
+              <div className="answer">
+                {quiz.answers[1].text}
+                <button>
+                  Select
+                </button>
+              </div>} */}
 
             <button id="submitAnswerBtn">Submit answer</button>
           </>

aa765b0599a93b3b851aa650f3505d70f350f10b
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-03-02 12:04:41 -0700

    working on select answer

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -4,7 +4,7 @@ import * as actionCreators from '../state/action-creators'
 import axios from 'axios'
 
 export function Quiz(props) {
-  const { quiz } = props
+  const { quiz, selectedAnswer } = props
   console.log(props)
   // need to get rid of useEffect and replace it with an 
   // if else statement for if quiz is loaded
@@ -12,50 +12,78 @@ export function Quiz(props) {
     props.fetchQuiz()
   }, [])
 
-  const onSelect = evt => {
-    console.log(props)
-    props.selectAnswer()
-  }
+  // const onSelect = evt => {
+  //   console.log(props)
+  //   const option = props.quiz.answers.answer_id
+  //   props.selectAnswer()
+  // }
+
+  console.log(props)
 
   return (
     <div id="wrapper">
+    {/* <h2>{quiz.question}</h2> */}
       {
         // quiz already in state? Let's use that, otherwise render "Loading next quiz..."
         quiz ? (
-          <>
-            <h2>{quiz.question}</h2>
-
-              {props.selectedAnswer?<div className="answer selected">
-                {quiz.answers[0].text}
+          quiz.answers.map(answer => {
+            return (
+              (selectedAnswer ? (<>
+                <div className="answer selected" key={answer.answer_id}>
+                {answer.text}
                 <button>
                   SELECTED
                 </button>
-              </div>
-              :
-              <div className="answer">
-                {quiz.answers[0].text}
-                <button onClick={() => onSelect()}>
-                  Select
-                </button>
-              </div>}
+                </div>
+               </>)
+               :
+               (<>
+                <div className="answer" key={answer.answer_id}>
+                  {answer.text}
+                  <button onClick={() => props.selectAnswer(answer.answer_id)}>
+                    Select
+                  </button>
+                </div>
+               </>))
+            )
+          })
+        )
+        // (
+        //   <>
+        //     <h2>{quiz.question}</h2>
 
-              {props.selectedAnswer?<div className="answer selected">
-                {quiz.answers[1].text}
-                <button>
-                  SELECTED
-                </button>
-              </div>
-              :
-              <div className="answer">
-                {quiz.answers[1].text}
-                <button onClick={() => onSelect()}>
-                  Select
-                </button>
-              </div>}
+        //       {props.selectedAnswer?<div className="answer selected">
+        //         {quiz.answers[0].text}
+        //         <button>
+        //           SELECTED
+        //         </button>
+        //       </div>
+        //       :
+              // <div className="answer">
+              //   {quiz.answers[0].text}
+              //   <button onClick={() => onSelect()}>
+              //     Select
+              //   </button>
+              // </div>}
 
-            <button id="submitAnswerBtn">Submit answer</button>
-          </>
-        ) : 'Loading next quiz...'
+        //       {props.selectedAnswer?<div className="answer selected">
+        //         {quiz.answers[1].text}
+        //         <button>
+        //           SELECTED
+        //         </button>
+        //       </div>
+        //       :
+        //       <div className="answer">
+        //         {quiz.answers[1].text}
+        //         <button onClick={() => onSelect()}>
+        //           Select
+        //         </button>
+        //       </div>}
+
+        //     <button id="submitAnswerBtn">Submit answer</button>
+        //   </>
+        // ) 
+        : 'Loading next quiz...'
       }
     </div>
   )

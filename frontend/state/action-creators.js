a9c046a63d251cae96c005e577a218cad290dc6c
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-03-03 11:50:05 -0700

    changed action creator for form to send over the name of the field

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -26,7 +26,7 @@ export function setQuiz(quiz) {
  }
 
 export function inputChange(value, inputId) {
-return ({ type: types.INPUT_CHANGE, payload: { value: value, [inputId]: value }})
+return ({ type: types.INPUT_CHANGE, payload: { value: value, [inputId]: inputId }})
  }
 
 export function resetForm() { 
